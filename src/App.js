9caf38e73d1a462616c6dbc91d9733e6fa755d32
sergey <sergey.zatsialepin@gmail.com>
2023-11-25 14:32:54 +0300

    add players

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import Player from "./components/Player";
+
 function App() {
   return (
     <main>
       <div className="game-container">
-        players
+        <ol className="players">
+          <Player name="Player 1" symbol="X"/>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
 
         gameBoard
       </div>
---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+const Player = ({name, symbol}) => {
+
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button>Edit</button>
+    </li>
+  );
+};
+
+export default Player;
